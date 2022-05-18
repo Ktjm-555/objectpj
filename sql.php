2efba64af4e158deabb69985e7b32f81d47a399c
“Ktjm-555” <m250919kaho@gmail.com>
2022-05-18 15:29:59 +0900

    POD削除

--- sql.php
+++ sql.php
@@ -6,7 +6,7 @@ Class Sql
 	// 一番最初に読む　コンストラクタ
 	function __construct() 
 	{
-		$db = new PDO('mysql:dbname=heroku_963ac50667f4cd7;host=us-cdbr-east-05.cleardb.net;charset=utf8','b40e7da217ecbc','43749863');
+		$db = mysqli_connect('us-cdbr-east-05.cleardb.net', 'b40e7da217ecbc', '43749863', 'heroku_963ac50667f4cd7');
 		if (!$db) {
 			die('接続失敗です。');
 		} else {
